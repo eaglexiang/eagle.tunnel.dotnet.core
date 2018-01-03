9508e2e973cc09513a0518ac19c4421d3df2baad
eaglexiang <eagle.xiang@outlook.com>
2018-01-03 15:10:47 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eagle.tunnel.dotnet.core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
