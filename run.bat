c4dab73faaa52b29104e00c14e298ec8ef984db7
eaglexiang <eagle.xiang@outlook.com>
2018-03-21 16:50:40 +0800

    add: scripts run.bat, run.sh

---
+++ run.bat
@@ -0,0 +1,1 @@
+dotnet .\\bin\\eagle.tunnel.dotnet.core.dll c=.\\scripts\\eagle-tunnel.conf sc hc
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+dotnet ./bin/eagle.tunnel.dotnet.core.dll c=./scripts/eagle-tunnel.conf sc hc
