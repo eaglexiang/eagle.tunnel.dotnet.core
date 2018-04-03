2b4fbb9b7bb1e2e65dd511e91f16f5d196fc258b
eaglexiang <eagle.xiang@outlook.com>
2018-04-04 04:34:48 +0800

    update: run.bat and run.sh

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-dotnet .\\publish\\usr\\lib\\eagle-tunnel\\eagle.tunnel.dotnet.core.dll c=.\\scripts\\eagle-tunnel.conf sc hc
+dotnet .\\publish\\usr\\lib\\eagle-tunnel\\eagle.tunnel.dotnet.core.dll .\\etc\\eagle-tunnel.conf
