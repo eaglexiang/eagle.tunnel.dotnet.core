5684c5cfaad9188dcd13189d22117b3b930e61ca
eaglexiang <eagle.xiang@outlook.com>
2018-04-06 21:45:15 +0800

    not cover etc and more

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 if [ $(whoami) = "root" ]; then
     WorkSpace=$(pwd)
-    cp -rf ${WorkSpace}/publish/* /
+    cp -rf ${WorkSpace}/publish/usr/* /usr/
+    cp -r ${WorkSpace}/publish/etc/* /etc/
     ln -sf /usr/lib/eagle-tunnel/run.sh /usr/bin/eagle-tunnel
     systemctl daemon-reload
 else
