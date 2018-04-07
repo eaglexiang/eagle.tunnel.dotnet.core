a0047ac6b7df04de6e55361c8aa15eb4da026e30
eaglexiang <eagle.xiang@outlook.com>
2018-04-07 18:34:26 +0800

    rm lib

--- src/lib/EagleTunnelArgs.cs
+++
@@ -1,9 +0,0 @@
-using System.Net;
-
-namespace eagle.tunnel.dotnet.core {
-    public class EagleTunnelArgs {
-        public string Domain { get; set; }
-        public IPAddress IP { get; set; }
-        public IPEndPoint EndPoint { get; set; }
-    }
-}
