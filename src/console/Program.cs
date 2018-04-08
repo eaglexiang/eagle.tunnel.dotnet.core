f6272b6da06ea1365d31c033263861e7b6db9a74
eaglexiang <eagle.xiang@outlook.com>
2018-04-08 19:35:02 +0800

    add: version

--- src/console/Program.cs
+++ src/console/Program.cs
@@ -3,13 +3,25 @@ using System.Threading;
 
 namespace eagle.tunnel.dotnet.core {
     class Program {
-        static void Main (string[] args) {
+        private static string version = "2.0.0";
+        public static void Main (string[] args) {
             if (args.Length >= 1) {
                 Conf.Init (args[0]);
             } else {
                 Conf.Init ();
             }
             Server.Start (Conf.LocalAddresses);
+        }
+
+        public static string Version()
+        {
+            return version;
+        }
+
+        private static void PrintVersion()
+        {
+            Console.WriteLine("UI Version: {0}", Version());
+            Console.WriteLine("Lib Version: {0}", Server.Version());
         }
     }
 }
