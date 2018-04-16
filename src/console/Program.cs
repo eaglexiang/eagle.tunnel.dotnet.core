af90ba64bb55892faf1264083f45fb6dfeb8f8bb
eaglexiang <eagle.xiang@outlook.com>
2018-04-16 23:28:06 +0800

    add: -h add: update.md

--- src/console/Program.cs
+++ src/console/Program.cs
@@ -3,25 +3,40 @@ using System.Threading;
 
 namespace eagle.tunnel.dotnet.core {
     class Program {
-        private static string version = "2.0.0";
+        private static string version = "2.1.0";
         public static void Main (string[] args) {
             if (args.Length >= 1) {
-                Conf.Init (args[0]);
+                if (args[0] == "-v") {
+                    PrintVersion();
+                }
+                else
+                {
+                    Conf.Init (args[0]);
+                    Server.Start (Conf.LocalAddresses);
+                }
             } else {
-                Conf.Init ();
+                PrintGuide();
             }
-            Server.Start (Conf.LocalAddresses);
         }
 
-        public static string Version()
-        {
+        public static string Version () {
             return version;
         }
 
-        private static void PrintVersion()
-        {
-            Console.WriteLine("UI Version: {0}", Version());
-            Console.WriteLine("Lib Version: {0}", Server.Version());
+        private static void PrintVersion () {
+            Console.WriteLine("Eagle Tunnel\n");
+            Console.WriteLine ("UI Version: {0}", Version ());
+            Console.WriteLine ("Lib Version: {0}\n", Server.Version ());
+        }
+
+        private static void PrintGuide () {
+            PrintVersion();
+            Console.WriteLine("usage: ");
+            Console.WriteLine("dotnet eagle.tunnel.dotnet.dll [option]\n");
+            Console.WriteLine("options:");
+            Console.WriteLine("[file path]\trun eagle tunnel with specific configuration file.");
+            Console.WriteLine("-h\tshow this guide.");
+            Console.WriteLine("-v\tshow version.");
         }
     }
 }
